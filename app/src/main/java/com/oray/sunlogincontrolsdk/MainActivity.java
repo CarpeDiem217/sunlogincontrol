7577e0ead0113a9026a3bab03d28c8940daba7c1
qianwei <qianwei@oray.com>
2024-05-30 17:50:54 +0800

    feature:修改编译报错

--- app/src/main/java/com/oray/sunlogincontrolsdk/MainActivity.java
+++ app/src/main/java/com/oray/sunlogincontrolsdk/MainActivity.java
@@ -1,12 +1,9 @@
 package com.oray.sunlogincontrolsdk;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.oray.controlsdk.TestFunction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.get_function).setOnClickListener(v -> {
-            Toast.makeText(this, TestFunction.getTestFunction(), Toast.LENGTH_SHORT).show();
-        });
     }
 }
